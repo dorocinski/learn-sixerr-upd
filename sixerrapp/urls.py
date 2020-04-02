c1cbbda4861ffec21db160399a4760b2eba27e3e
Luiz Dorocinski <dorocinski@gmail.com>
2020-04-02 15:01:01 -0300

    T9_1: Create new pages for gig

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -4,4 +4,6 @@ from sixerrapp import views
 urlpatterns = [
     path('', views.home, name='home'),
     re_path('gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
+    path('my_gigs/', views.my_gigs, name='my_gigs'),
+    path('create_gig/', views.create_gig, name='create_gig'),
 ]
