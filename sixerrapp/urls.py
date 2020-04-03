dc8e8acd89a1742276c296c8cf6be25613ef91b1
Luiz Dorocinski <dorocinski@gmail.com>
2020-04-03 17:55:48 -0300

    T12_2: Create functions for summary pages

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('edit_gig/<int:id>/', views.edit_gig, name='edit_gig'),
     path('profile/<slug:username>/', views.profile, name='profile'),
     path('checkout/', views.create_purchase, name='create_purchase'),
+    path('my_buyings/', views.my_buyings, name='my_buyings'),
+    path('my_sellings/', views.my_sellings, name='my_sellings'),
 ]
 
 # Help for URL PAtterns: https://consideratecode.com/2018/05/02/django-2-0-url-to-path-cheatsheet/
